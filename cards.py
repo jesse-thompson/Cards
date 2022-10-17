755753b99625c82cc8bacea9a25884e19b12a85c
Jesse Thompson <j13thompson@student.bridgew.edu>
2022-10-17 16:46:46 -0400

    Added TODOs

--- cards.py
+++ cards.py
@@ -25,13 +25,15 @@ class Card:
     def __str__(self):
         return 'rank: {} of suit: {}'.format(self.rank.value, self.suit.card_suit)
 
-
+#TODO: finish Hand
 class Hand:
     card = None
 
     def __init__(self, size):
         self.card = []
 
+#TODO: figure out card_value
+#TODO: figure out card_suit
         for rank in range(0, size + 1):
             for suit in range(rank, size + 1):
                 card_value =
@@ -55,6 +57,8 @@ class Hand:
     def get_card(self):
 
 
-
+#TODO: create_deck (separate from Hand class?)
+#TODO: get_card
+#TODO: create test(s)
 hand = Hand(5)
 hand.print_hand()
