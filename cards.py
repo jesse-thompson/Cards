798385bee9f8298e1b6840043f58ecb456c32abd
Jesse Thompson <j13thompson@student.bridgew.edu>
2022-10-18 00:04:41 -0400

    Fixed Deck, Card, and removed extraneous variable declarations

--- cards.py
+++ cards.py
@@ -3,9 +3,6 @@ import random
 
 # class for the rank of the card: 2-10, Jack, King, Queen, Ace
 class Rank:
-    value = None
-    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
     def __init__(self, rank_value, icon):
         self.rank_value = rank_value
         self.icon = icon
@@ -13,58 +10,77 @@ class Rank:
 
 # class for the suit of the card: Spade, Heart, Diamond, Club
 class Suit:
-    card_suit = None
-    suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
-
     def __init__(self, suit_name):
         self.card_suit = suit_name
 
 
 # class to make a card using rank and suit
 class Card:
-    def __init__(self, rank, suit):
+    def __init__(self, rank, icon, suit):
         self.rank = rank
+        self.icon = icon
         self.suit = suit
 
     def __str__(self):
-        return '{} of {}'.format(self.rank.icon, self.suit)
+        return '{} of {}'.format(self.rank.icon, self.suit.card_suit)
 
 
 # class for the deck of 52 cards
 class Deck:
-    card = None
+    card_rank = None
+    card_icon = None
+    card_suit = None
 
     def __init__(self):
-        self.card = []
+        self.deck_of_cards = []
 
-        for rank in range(0, 52):
-            for suit in range(rank, 52):
-                card_value = Rank(rank)
-                card_suit = Suit(suit)
-                new_card = Card(card_value, card_suit)
-                self.add(new_card)
+        for rank in range(1, 53):
+            card_rank = rank
+
+            if rank % 13 == 0:
+                card_icon = 'A'
+            elif 0 < rank % 13 < 10:
+                card_icon = rank + 1
+            elif rank == 10:
+                card_icon = 'J'
+            elif rank == 11:
+                card_icon = 'Q'
+            elif rank == 12:
+                card_icon = 'K'
+
+            if rank < 13:
+                card_suit = 'Spades'
+            elif 13 <= rank < 26:
+                card_suit = 'Hearts'
+            elif 26 <= rank < 39:
+                card_suit = 'Clubs'
+            elif 39 <= rank < 52:
+                card_suit = 'Diamonds'
+
+            new_card = Card(card_rank, card_icon, card_suit)
+            self.add(new_card)
 
     # adds card to deck
     def add(self, card):
-        self.card.append(card)
+        self.deck_of_cards.append(card)
 
     # creates a deck of 52 cards
     def create_deck(self):
-        return self
+        return self.deck_of_cards
 
     # shuffles the current deck, rearranging them in a random order
-    def shuffle_deck(self):
-        random.shuffle(self)
+    def shuffle_deck(self, deck_to_shuffle):
+        random.shuffle(deck_to_shuffle)
 
     # returns the top card from the deck, each call returns the next card in order
     def get_card(self):
-        return self.card.pop()
+        top_card = self.deck_of_cards[0]
+        self.deck_of_cards = self.deck_of_cards[1:]
+        return top_card
 
 
 # class to create a hand, accepting various sizes
 class Hand:
-    card = None
-
     def __init__(self, size):
         self.card = []
 
@@ -81,7 +97,7 @@ class Hand:
         self.card.append(card)
 
 
-deck = Deck.create_deck(self=Deck)
+deck = Deck.create_deck()
 deck.shuffle_deck()
 hand = Hand(5)
 hand.print_hand()
