1cc0058e710f8c1bbdf62fa58e797372dbeff9fc
Jesse Thompson <j13thompson@student.bridgew.edu>
2022-10-30 16:48:02 -0400

    Added: - features dir  - steps dir   - cards_tests.feature (has some scenarios) - cards_tests.py
    With some structure, but not really started

---
+++ cards_tests.py
@@ -0,0 +1,39 @@
+from behave import *
+import cards
+
+
+@Given('I have created a new card object')
+def step_impl(context):
+
+
+@When('I create the new class')
+def step_impl(context):
+
+
+@Then('the card value should be recorded')
+def step_impl(context):
+
+
+@Given('I have a set number of cards')
+def step_impl(context):
+
+
+@When('I draw a card')
+def step_impl(context):
+
+
+@Then('it is removed from the deck')
+def step_impl(context):
+
+
+@Given('I have a deck of cards')
+def step_impl(context):
+
+
+@When('I shuffle them')
+def step_impl(context):
+
+
+@Then('The order has changed')
+def step_impl(context):
+
